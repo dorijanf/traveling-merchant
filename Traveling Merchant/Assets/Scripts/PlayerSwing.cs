068c2350d91426a600877446fb8d57e573f2a47e
dorijanf <dorijan.fabijanic@hotmail.com>
2019-09-26 01:38:00 +0200

    Fixed targeting system
    Fixed targeting system, it is now completely done.

--- Traveling Merchant/Assets/Scripts/PlayerSwing.cs
+++ Traveling Merchant/Assets/Scripts/PlayerSwing.cs
@@ -6,7 +6,7 @@ public class PlayerSwing : MonoBehaviour
     public LayerMask whatIsTarget;
     public Vector3 offset;
     private float timeBetweenAttacks;
-    private float degrees;
+    public float degrees;
 
     [Space]
     [Header("Character Attributes:")]
@@ -21,7 +21,6 @@ public class PlayerSwing : MonoBehaviour
 
     private void Update()
     {
-        ChangeRotation();
         if (timeBetweenAttacks <= 0)
         {
             Swing();
@@ -34,9 +33,10 @@ public class PlayerSwing : MonoBehaviour
 
     private void Swing()
     {
+        ChangeRotation();
+        DebugDrawBox(targetPos.position + offset, new Vector2(boxSizeX, boxSizeY), degrees, Color.red, 0.1f);
         if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("KEY PRESSED.");
             Collider2D[] targetsToDamage = Physics2D.OverlapBoxAll(targetPos.position + offset, new Vector2(boxSizeX, boxSizeY), degrees, whatIsTarget);
             for (int i = 0; i < targetsToDamage.Length; i++)
             {
@@ -55,27 +55,47 @@ public class PlayerSwing : MonoBehaviour
     {
         if (transform.parent.GetComponent<CheckPlayerDirection>().IsMovingNorth())
         {
-            RotateBoxCollider(180f);
-
+            offset = new Vector3(0, 0.25f, 0);
+            degrees = 180f;
         }
         else if (transform.parent.GetComponent<CheckPlayerDirection>().IsMovingWest())
         {
-            RotateBoxCollider(270f);
+            offset = new Vector3(-0.25f, 0, 0);
+            degrees = 270f;
         }
         else if (transform.parent.GetComponent<CheckPlayerDirection>().IsMovingEast())
         {
-            RotateBoxCollider(90f);
+            offset = new Vector3(0.25f, 0, 0);
+            degrees = 90;
         }
         else if (transform.parent.GetComponent<CheckPlayerDirection>().IsMovingSouth())
         {
-            RotateBoxCollider(0f);
+            offset = new Vector3(0, -0.25f, 0);
+            degrees = 0;
         }
+
+        transform.rotation = Quaternion.Euler(Vector3.forward * degrees);
     }
 
-    private void OnDrawGizmosSelected()
+    void DebugDrawBox(Vector2 point, Vector2 size, float angle, Color color, float duration)
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(targetPos.position + offset, new Vector2(boxSizeX, boxSizeY));
-        
+
+        var orientation = Quaternion.Euler(0, 0, angle);
+
+        // Basis vectors, half the size in each direction from the center.
+        Vector2 right = orientation * Vector2.right * size.x / 2f;
+        Vector2 up = orientation * Vector2.up * size.y / 2f;
+
+        // Four box corners.
+        var topLeft = point + up - right;
+        var topRight = point + up + right;
+        var bottomRight = point - up + right;
+        var bottomLeft = point - up - right;
+
+        // Now we've reduced the problem to drawing lines.
+        Debug.DrawLine(topLeft, topRight, color, duration);
+        Debug.DrawLine(topRight, bottomRight, color, duration);
+        Debug.DrawLine(bottomRight, bottomLeft, color, duration);
+        Debug.DrawLine(bottomLeft, topLeft, color, duration);
     }
 }
